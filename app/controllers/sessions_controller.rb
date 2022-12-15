cb24f2f86d19dce8190cc5234e4e96286af56d23
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-15 15:53:07 +0900

    ログインの仕組みを実装:ログインシステム10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,4 +5,12 @@ end
 
 def create
   user = User.find_by(email: params[:session][:email].downcase)
+  if user && user.authenticate(params[:session][:password])
+    # ログイン成功した場合
+  else
+    flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    # ログイン失敗した場合
+    #フラッシュメッセージを追加して、new.html.erbをレンダリング
+  end
 end
