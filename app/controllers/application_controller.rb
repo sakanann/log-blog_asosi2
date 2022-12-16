36c668fd0aa260d56ee2bea1e49adc92f7ddbd48
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-16 19:16:22 +0900

    ログインシステム13まで製作

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   include SessionsHelper
-  #Applicationコントローラにこのモジュールをインクルードする（含める）ことによって、Railsの全コントローラでこのモジュールが使用できるようになる
+  before_action :login_required
+
+  private
+
+  def login_required
+    redirect_to new_session_path unless current_user
+  end
 end
