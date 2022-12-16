13bb9e531dc5b7eb63837c96ce7d4579b925d018
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-16 18:56:06 +0900

    Userコントローラのnew、createアクションに対してlogin_requiredメソッドが呼ばれないよう設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper
+  #Applicationコントローラにこのモジュールをインクルードする（含める）ことによって、Railsの全コントローラでこのモジュールが使用できるようになる
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,26 @@
 class SessionsController < ApplicationController
+  
+  skip_before_action :login_required, only: [:new, :create]
   def new
   end
-end
 
-def create
-  user = User.find_by(email: params[:session][:email].downcase)
-  if user && user.authenticate(params[:session][:password])
-    # ログイン成功した場合
-  else
-    flash.now[:danger] = 'ログインに失敗しました'
-      render :new
-    # ログイン失敗した場合
-    #フラッシュメッセージを追加して、new.html.erbをレンダリング
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user.id)
+      # ログイン成功した場合
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+        render :new
+      # ログイン失敗した場合
+      #フラッシュメッセージを追加して、new.html.erbをレンダリング
+    end
+  end
+
+  def destroy
+    session.delete(:user_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to new_session_path
   end
 end
