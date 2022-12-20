069d3f5f8d48c8f48dc2b480f2f7c7ea1a89439f
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-20 18:43:15 +0900

    テキストに沿ってCRUD

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,2 +1,24 @@
 class BlogsController < ApplicationController
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+    redirect_to new_blog_path
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    Blog.create(blog_params)
+    redirect_to new_blog_path
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
