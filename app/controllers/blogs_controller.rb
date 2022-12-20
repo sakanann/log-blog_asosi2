d53cfc8858895d4349fb0e084599add1b1756bc9
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-21 02:49:05 +0900

    helperメソッドを作成_form_withの中をhelperメソッドに置き換

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,17 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy]
+
   def index
     @blogs = Blog.all
   end
 
   def show
-    @blog = Blog.find(params[:id])
   end
 
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "ブログを編集しました！"
     else
@@ -24,6 +22,11 @@ class BlogsController < ApplicationController
   def destroy
     @blog.destroy
     redirect_to blogs_path, notice:"ブログを削除しました！"
+  end
+
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
 
   def new
@@ -46,6 +49,6 @@ class BlogsController < ApplicationController
   end
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    
   end
 end
