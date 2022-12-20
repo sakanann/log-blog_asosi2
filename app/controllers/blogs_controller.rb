38f61485444fd7c19b987d8d6422fc34787c8f16
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-21 01:13:29 +0900

    パーシャル作成_new&edit記述共通化_renderメソッドで呼び出し

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,6 +7,19 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
   def new
     @blog = Blog.new
   end
@@ -24,5 +37,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
