2f647d1d86885e1f6a099074e6d99542186d80d8
takuya sakamoto <sthtcwm@yahoo.co.jp>
2022-12-21 12:10:04 +0900

    add:ブログ　タイトルと内容文字制限　20＿1-140文字バリデーション

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
-  validates :content, presence: true
+  validates :title, presence: true,length:{maximum: 20}
+  validates :content, presence: true,length:{in:1..140}
 end
